5d570cf184643912ed0d7ab804fc9261b0a1cdf2
unknown <eksesahra468@gmail.com>
2025-10-19 22:55:27 +0700

    first commit

---
+++ admin/produk.php
@@ -0,0 +1,32 @@
+<h2>Data Produk</h2>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Harga</th>
+            <th>Berat</th>
+            <th>Foto</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $nomor=1; ?>
+        <?php $ambil=$koneksi->query("SELECT * FROM produk"); ?>
+        <?php while($pecah = $ambil->fetch_assoc()){ ?>
+        <tr>
+            <td><?php echo $nomor; ?></td>
+            <td><?php echo $pecah['nama_produk']; ?></td>
+            <td><?php echo $pecah['harga_produk']; ?></td>
+            <td><?php echo $pecah['berat_produk']; ?></td>
+            <td><?php echo $pecah['foto_produk']; ?></td>
+            <td>
+                <a href="" class="btn-danger btn">hapus</a>
+                <a href="" class="btn-warning btn">ubah</a>
+            </td>
+        </tr>
+        <?php $nomor++; ?>
+        <?php } ?>
+    </tbody>
+</table>
